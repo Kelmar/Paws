2bfce902f6368c0dbcaccef7c15b00dc14ccc111
Kelmar Firesun <kfiresun@gmail.com>
2018-12-26 11:56:11 -0600

    More reorganization for Lepton

--- src/lepton/lifecycle.ts
+++ src/lepton/lifecycle.ts
@@ -5,7 +5,31 @@ import { IDisposable } from "./interfaces";
 
 /* ================================================================================================================= */
 
-export function using(item: IDisposable, fn: (item: IDisposable) => void): void
+export enum Lifetime
+{
+    Transient,
+    Scoped,
+    Singleton
+}
+
+/* ================================================================================================================= */
+/**
+ * Guarantees object disposal.
+ *
+ * The callback is called with the given item.  Once complete, the item's dispose() method is called.
+ *
+ * If the supplied item value is null, the callback will still be called, but the dispose() method will not be called.
+ *
+ * @param item The item to dispose
+ * @param fn The function to call on the item.
+ *
+ * @example using(myObj, item =>
+ * {
+ *     // item is the same reference as myObj.
+ *     item.doThings();
+ * }); // dispose() method on myObj, called here.
+ */
+export function using<T extends IDisposable>(item: T, fn: (item: T) => void): void
 {
     try
     {
